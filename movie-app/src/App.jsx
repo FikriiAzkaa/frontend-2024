602f776581f800b82f5080e6d734250575aad5a4
fasska <fasska880@gmail.com>
2024-03-26 13:38:17 +0700

    pertemuan4

---
+++ movie-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header.jsx'
+import Main from './components/Main.jsx'
+import Footer from './components/Footer.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default App
